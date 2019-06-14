c6fae6e335bb93e882877a68fba4438d6779c2b5
AaronClifford <aclifforda@gmail.com>
2019-06-14 14:51:37 +0100

    Update Config.php backwards compatability with PHP
    Patched to support older versions of PHP.

--- config/Config.php
+++ config/Config.php
@@ -1,20 +1,19 @@
 <?php
-return [
-    'dac' => [
-        'host' => 'testbed-dac.nominet.org.uk',
-        'port' => '3043',
-    ],
-    'epp' => [
-        'host' => 'ssl://testbed-epp.nominet.org.uk',
-        'port' => '700',
-        'tag' => 'YOURTAG',
-        'password' => 'YOURPASSWORD',
-        'testRegistrantID' => '', // Max 16 characters, change this every time you run the script.
-        'testRegistrantName' => '',
-        'testRegistrantOrg' => '',
-        'testDomain' => '', // Must be a domain
-    ],
-    'settings' => [
-        'debug' => false
-    ]
-];
+return array(
+    'dac' =>
+        array('host' => 'testbed-dac.nominet.org.uk', 'port' => '3043'),
+
+    'epp' =>
+        array('host' => 'ssl://testbed-epp.nominet.org.uk',
+            'port' => '700',
+            'tag' => 'YOURTAG',
+            'password' => 'YOURPASSWORD',
+            'testRegistrantID' => '', // Max 16 characters, change this every time you run the script.
+            'testRegistrantName' => '',
+            'testRegistrantOrg' => '',
+            'testDomain' => ''), // Must be a domain on the ROR list to successfully complete the Nominet requirements
+
+    'settings' =>
+        array('debug' => false)
+
+);
